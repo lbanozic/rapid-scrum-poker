40eb9e0bf176fe303ad4355487487269fccb21bc
lbanozic <lubanozic@gmail.com>
2022-06-04 17:34:46 +0200

    Update dependencies

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,20 @@
-import { ColorModeScript } from "@chakra-ui/react";
 import * as React from "react";
-import ReactDOM from "react-dom";
+import { ColorModeScript } from "@chakra-ui/react";
+import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { App } from "./App";
 import reportWebVitals from "./reportWebVitals";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(
+const container = document.getElementById("root");
+const root = createRoot(container!);
+root.render(
   <React.StrictMode>
     <BrowserRouter>
       <ColorModeScript />
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
 
 // If you want your app to work offline and load faster, you can change
