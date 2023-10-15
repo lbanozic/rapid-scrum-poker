09ed4c4ce6d5797009a0b9bed2af5ff22d48734d
lbanozic <lubanozic@gmail.com>
2023-10-15 16:23:53 +0200

    Add toast info to the user about spinning up the server

--- src/game/GameStarter.tsx
+++ src/game/GameStarter.tsx
@@ -1,5 +1,6 @@
-import { Button, Center } from "@chakra-ui/react";
+import { Button, Center, useToast } from "@chakra-ui/react";
 import { nanoid } from "nanoid";
+import { useEffect, useState } from "react";
 import { GoZap } from "react-icons/go";
 
 /**
@@ -16,6 +17,14 @@ export default function GameStarter(props: {
    */
   onGameStarted?: (gameId: string) => void;
 }) {
+  // initialize the toast
+  const toast = useToast();
+
+  // initialize initial start info timeout state
+  const [initialStartInfoTimeoutId, setInitialStartInfoTimeoutId] = useState<
+    number | null
+  >(null);
+
   /**
    * Generates new game id and calls the game started prop function
    */
@@ -25,7 +34,31 @@ export default function GameStarter(props: {
 
     // call game started prop function if it exists
     props.onGameStarted && props.onGameStarted(gameId);
+
+    // after 5 seconds, inform user that initial game start may take a while by showing toast message
+    const timeoutId = window.setTimeout(() => {
+      toast({
+        title: "Warming Things Up...",
+        description:
+          "Our servers are getting ready to start your game. This initial start might take up to 30 seconds. Thanks for your patience!",
+        status: "info",
+        duration: 9000,
+        isClosable: true,
+      });
+    }, 5000);
+
+    // set timeout id so it can be cleared
+    setInitialStartInfoTimeoutId(timeoutId);
   }
+
+  // clear initial start info timeout when the component unmounts so the toast message isn't shown when the game starts before timeout
+  useEffect(() => {
+    return () => {
+      if (initialStartInfoTimeoutId) {
+        window.clearTimeout(initialStartInfoTimeoutId);
+      }
+    };
+  }, [initialStartInfoTimeoutId]);
 
   return (
     <Center height="90vh">
